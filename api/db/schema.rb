7b887aef7f8d074d72e92a287e41a85572b3c521
Graham Otte <graham.otte@gmail.com>
2020-01-19 21:27:47 -0800

    add delayed job

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_20_050648) do
+ActiveRecord::Schema.define(version: 2020_01_20_052731) do
+
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer "priority", default: 0, null: false
+    t.integer "attempts", default: 0, null: false
+    t.text "handler", null: false
+    t.text "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string "locked_by"
+    t.string "queue"
+    t.datetime "created_at", precision: 6
+    t.datetime "updated_at", precision: 6
+    t.index ["priority", "run_at"], name: "delayed_jobs_priority"
+  end
 
   create_table "subscriptions", force: :cascade do |t|
     t.string "channel_id"
