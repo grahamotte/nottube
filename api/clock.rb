224056d0d33f4e0f4786336d3a93b352270f61dc
Graham Otte <graham.otte@gmail.com>
2020-01-26 21:26:23 -0800

    update less freq bc yt sucks

--- api/clock.rb
+++ api/clock.rb
@@ -4,6 +4,6 @@ require './config/boot'
 require './config/environment'
 
 module Clockwork
-  every(1.hour, 'sync_most_recent_videos') { SyncMostRecentVideosJob.perform_later }
-  every(4.hours, 'refresh_subscription_metadata') { RefreshSubscriptionMetadataJob.perform_later }
+  every(4.hour, 'sync_most_recent_videos') { SyncMostRecentVideosJob.perform_later }
+  every(6.hours, 'refresh_subscription_metadata') { RefreshSubscriptionMetadataJob.perform_later }
 end
